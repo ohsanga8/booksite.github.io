d2de9bceada05d26e4b3e01fecb52e46af5d0866
ohsanga8 <ohsanga8@Naver.com>
2022-12-06 23:56:59 +0900

    second commit

---
+++ door/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.start, name='start'),
+    path('question/', views.question, name='question'),
+    path('answer/', views.answer, name='answer'),
+    path('answers/', views.answers, name='answers'),
+]
--- door/views.py
+++ door/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#시작 화면
+def start(request):
+    return render(request, 'door/start.html')
+
+#분야 선택 & 질문 입력
+def question(request):
+    return render(request, 'door/question.html')
+
+#결과
+def answer(request):
+    return render(request, 'door/answer.html')
+
+#이전 문답
+def answers(request):
+    return render(request, 'door/answer_list.html')
